ad8a32f4e3db2a60659bc28b5b9cc9fa406ae73e
jng90 <jng@o2.pl>
2021-10-24 11:08:21 +0200

    MultiplicationTable a2

--- src/components/MultiplicationTable.js
+++ src/components/MultiplicationTable.js
@@ -2,11 +2,25 @@ import {useState} from "react";
 
 const MultiplicationTable = () => {
 
+    //zadeklarowalismy zmienna 'number' oraz funkcje 'setNumbers' (setter)
+    const [number, setNumber] = useState(0);
+    console.log(number);
 
+    let wiersze = [];
+    for (let i = 0; i < number; i++) {
+        let kolumny = [];
+        for (let j = 0; j < number; j++) {
+            kolumny.push(((i+1) * (j+1)));
+        }
+        wiersze.push(kolumny);
+    }
+// Dla liczby 3
+// wiersze =  [
+//              [ 1, 2, 3 ]
+//              [ 2, 4, 6 ]
+//              [ 3, 6, 9 ]
+//            ]
 
-    //zadeklarowalismy zmienna 'numbers' oraz funkcje 'setNumbers' (setter)
-    const [numbers, setNumbers] = useState([])
-    console.log(numbers);
 
     return (<div className="multiplication-table">
         <input type="number" id='number_field'/>
@@ -15,24 +29,21 @@ const MultiplicationTable = () => {
             let number = document.getElementById('number_field').value
             console.log(number);
 
-            let copiedNumbers = [...numbers];   // przepisanie kolekcji
-            copiedNumbers.push(number);    // dodanie do kolekcji elementu
-
-            setNumbers(copiedNumbers);    // podmiana kolekcji
+            setNumber(number);    // podmiana kolekcji
 
         }}>
             Kliknij mnie
         </button>
         <table>
-            <thead>
-            <tr>
-                <th>Liczba</th>
-            </tr>
-            </thead>
             <tbody>
             {
-                numbers.map(value => {
-                    return (<tr key={value}><td>{value}</td></tr>)
+                wiersze.map(wiersz => {
+                    // [1, 2, 3]
+                    const kolumny = wiersz.map(kolumna => {
+                        return (<td key={kolumna}>{kolumna}</td>)
+                    });
+
+                    return (<tr key={wiersz}>{kolumny}</tr>)
                 })
             }
             </tbody>
